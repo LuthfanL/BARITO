fe51ed95c91b7c4704024efdff169397ed8f1293
Muhammad Luthfan L <upan.azuardi@gmail.com>
2025-02-01 20:44:38 +0700

    Fix logo website

--- resources/views/riwayatBookingTenant.blade.php
+++ resources/views/riwayatBookingTenant.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/favicon-96x96.png') }}">
     <title>Riwayat Booking Tenant</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMJTVF1a1wMA2gO/YHbx+fyfJhN/0Q5ntv7zYY" crossorigin="anonymous">
@@ -235,7 +236,7 @@
             <h3 class="font-semibold text-gray-900">Alasan Penolakan</h3>
         </div>
         <div class="px-3 py-2">
-            <textarea id="keperluan-acara" rows="3" class="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm text-gray-500 p-3" readonly>{{ $booking->alasanPenolakan }}</textarea>
+            <textarea id="keperluan-acara" rows="3" class="mt-1 block w-full rounded-md border-gray-300 focus:border-blue-500 focus:ring-blue-500 sm:text-sm text-gray-500 p-3" readonly>{{ isset($booking) && !empty($booking->alasanPenolakan) ? $booking->alasanPenolakan : 'Tidak ada alasan penolakan.' }}</textarea>
         </div>
         <div data-popper-arrow></div>
     </div>
--- resources/views/verifikasiBookingTenant.blade.php
+++ resources/views/verifikasiBookingTenant.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{ asset('assets/favicon-96x96.png') }}">
     <title>Verifikasi Booking Tenant</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha384-k6RqeWeci5ZR/Lv4MR0sA0FfDOMJTVF1a1wMA2gO/YHbx+fyfJhN/0Q5ntv7zYY" crossorigin="anonymous">
